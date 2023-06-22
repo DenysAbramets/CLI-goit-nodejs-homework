9018b953046b7e63366f15cb65a67fd3fcedace7
DenysAbramets <abramets_denys@outlook.com>
2023-06-22 22:21:32 +0200

    done HW

---
+++ README.md
@@ -0,0 +1,4 @@
+https://i.ibb.co/fM8Xkt0/2023-06-22-221821.png
+https://i.ibb.co/sJYR1BK/2023-06-22-221846.png
+https://i.ibb.co/ZXC0dDZ/2023-06-22-221855.png
+https://i.ibb.co/YpS9W7y/2023-06-22-221902.png
---
+++ index.js
@@ -0,0 +1,32 @@
+// const yargs = require("yargs");
+// const { hideBin } = require("yargs/helpers");
+const contacts = require("./contacts");
+const argv = require("yargs").argv;
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.log(allContacts);
+      break;
+
+    case "get":
+      const getContact = await contacts.getContactById(id);
+      console.log(getContact);
+      break;
+
+    case "add":
+      const addContact = await contacts.addContact({ name, email, phone });
+      console.log(addContact);
+      break;
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+invokeAction(argv);
